255380bb84e2e76551c880ad16a9d8837e648c3e
prolic <prolic@users.noreply.github.com>
2025-05-27 22:49:02 +0000

    deploy: db6c367fd63f805d8e2cc507471e84efe381529e

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Heroic Features v5.0.6 (https://startbootstrap.com/template/heroic-features)
-* Copyright 2013-2024 Start Bootstrap
+* Copyright 2013-2025 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
 */
 // This file is intentionally blank
